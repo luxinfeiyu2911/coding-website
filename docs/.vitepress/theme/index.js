d206fc4ebdd467561bc1c4bb312f084f0ac7a3b0
鹿心肺语 <luxinfeiyu1129@163.com>
2025-02-12 20:59:02 +0800

    update: 添加文章回到顶部功能;

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,7 +2,11 @@
 import {h} from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+// code block group icons plugin
 import 'virtual:group-icons.css'
+// back-to-top plugin
+import vitepressBackToTop from 'vitepress-plugin-back-to-top'
+import 'vitepress-plugin-back-to-top/dist/style.css'
 
 /** @type {import('vitepress').Theme} */
 export default {
@@ -14,5 +18,9 @@ export default {
     },
     enhanceApp({app, router, siteData}) {
         // ...
+        vitepressBackToTop({
+            // default
+            threshold: 300
+        })
     }
 }
